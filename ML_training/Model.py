6e449fd26c4e53342fd1fd9ed08f83492ac5435a
nyv62 <nyv62@mst.edu>
2022-06-28 14:06:12 -0500

    Create and update Model.py

--- ML_training/Model.py
+++ ML_training/Model.py
@@ -36,6 +36,10 @@ x_train, scaler_x = standardized(x_train, 'MinMax')
 x_test = scaler_x.transform(x_test)
 y_train, scaler_y = standardized(y_train, 'MinMax')
 
+from joblib import dump, load
+dump(scaler_x, 'Scaler/US_WE_MinMax_scaler_x.bin', compress=True)
+dump(scaler_y, 'Scaler/US_WE_MinMax_scaler_y.bin', compress=True)
+
 es = EarlyStopping(verbose=1,patience=10)
 
 # Initialiizinig the ANN
@@ -52,11 +56,78 @@ model.add(tf.keras.layers.Dense(units=40,activation=PReLU(), kernel_initializer=
 model.add(tf.keras.layers.Dense(units=20,activation=PReLU(), kernel_initializer='he_uniform'))
 # Adding hidden layer
 model.add(tf.keras.layers.Dense(units=10,activation=PReLU(), kernel_initializer='he_uniform'))
+# Adding hidden layer
+model.add(tf.keras.layers.Dense(units=5,activation=PReLU(), kernel_initializer='he_uniform'))
 # Adding the output layer
 model.add(tf.keras.layers.Dense(units=1,activation='linear'))
 # Compilling the ANN
 model.compile(optimizer='adam',loss=['MSE'],metrics= ['MAE'])
 
-# Trarin the ANN on the Training set
+# Train the ANN on the Training set
 model.fit(x_train, y_train, batch_size=128, epochs=150, validation_split=0.2, callbacks=[es])
+
+# Plot history: MSE
+plt.plot(model.history.history['loss'], label='MSE (training data)')
+plt.plot(model.history.history['val_loss'], label='MSE (validation data)')
+plt.title('MSE for noise prediction')
+plt.ylabel('MSE value')
+plt.xlabel('No. epoch')
+plt.legend(loc="upper left")
+plt.savefig('MSE_history.png', dpi=300)
+
+# Plot history: MAE
+plt.plot(model.history.history['MAE'], label='MAE (training data)')
+plt.plot(model.history.history['val_MAE'], label='MAE (validation data)')
+plt.title('MAE for noise prediction')
+plt.ylabel('MAE value')
+plt.xlabel('No. epoch')
+plt.legend(loc="upper left")
+plt.savefig('MAE_history.png', dpi=300)
+
+# Saving model
+model.save('Model/US_WE_PTE_fixed_hgtlvs_cloud_model')
+
+# Predict different model
+predict = scaler_y.inverse_transform(model.predict(x_test))
+true = y_test.values
+
+from sklearn.metrics import mean_squared_error, r2_score
+print("ANN model")
+# The mean squared error
+print('Mean squared error: %.10f'% mean_squared_error(true, predict))
+
+# The R2 score
+print('R2: %.5f'% r2_score(true,predict))
+
+# The RMSE
+rmse = np.sqrt(mean_squared_error(true, predict))
+print('RMSE: %.5f' % rmse)
+
+errors = predict - true
+print('Average errror: %.5f' %np.mean(abs(errors)))
+
+# Plot of Observation vs Prediction
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='scatter_density')
+density = ax.scatter_density(true, predict, cmap=white_viridis)
+cbar = fig.colorbar(density)
+cbar.set_label(label='Number of points per pixel',size=10)
+ax.tick_params(axis='both',which='major',labelsize=10)
+plt.xlabel('Observed',fontsize=10)
+plt.ylabel('Predicted',fontsize=10)
+cbar.ax.tick_params(labelsize=10)
+plt.figsave('Ob_v_Pred.png', dpi=300)
+
+# Plot of residual of the prediction
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1, projection='scatter_density')
+density = ax.scatter_density(true, true-predict, cmap=white_viridis)
+cbar = fig.colorbar(density)
+cbar.set_label(label='Number of points per pixel',size=10)
+ax.tick_params(axis='both',which='major',labelsize=10)
+plt.xlabel('True',fontsize=10)
+plt.ylabel('Residual',fontsize=10)
+cbar.ax.tick_params(labelsize=10)
+plt.figsave('Resid_true.png', dpi=300)
+
 
